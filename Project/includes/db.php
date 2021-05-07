86b0c6b92ffd1dffdd4642eae19a44db2331b244
Samiya Kazi <samiya.kazi@northsouth.edu>
2021-05-07 11:35:26 +0600

    Added error message for database connection error
    Added error message for database connection error.

--- Project/includes/db.php
+++ Project/includes/db.php
@@ -7,4 +7,9 @@
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
+
+	// Check connection
+	if(!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
 ?>
